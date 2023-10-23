71adf0c576099a5488bcac044d5b51c0603656a9
Jaex <Jaex@users.noreply.github.com>
2023-10-23 16:16:35 +0300

    Code refactoring

--- Chrome/eventPage.js
+++ Chrome/eventPage.js
@@ -1,16 +1,42 @@
 chrome.runtime.onInstalled.addListener((details) => {
     chrome.contextMenus.create({
-        "id": "ShareX",
-        "title": "Upload with ShareX",
-        "contexts": ["selection", "image", "video", "audio"]
+        "id": "ShareX_Upload_Image",
+        "title": "Upload image with ShareX",
+        "contexts": ["image"]
+    });
+
+    chrome.contextMenus.create({
+        "id": "ShareX_Upload_Video",
+        "title": "Upload video with ShareX",
+        "contexts": ["video"]
+    });
+
+    chrome.contextMenus.create({
+        "id": "ShareX_Upload_Audio",
+        "title": "Upload audio with ShareX",
+        "contexts": ["audio"]
+    });
+
+    chrome.contextMenus.create({
+        "id": "ShareX_Upload_Text",
+        "title": "Upload text with ShareX",
+        "contexts": ["selection"]
     });
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
-    if (info.menuItemId === "ShareX") {
-        chrome.runtime.sendNativeMessage("com.getsharex.sharex", {
+    let application = "com.getsharex.sharex";
+
+    if (info.menuItemId === "ShareX_Upload_Image" ||
+        info.menuItemId === "ShareX_Upload_Video" ||
+        info.menuItemId === "ShareX_Upload_Video") {
+        chrome.runtime.sendNativeMessage(application, {
             MediaType: info.mediaType,
-            URL: info.srcUrl,
+            URL: info.srcUrl
+        });
+    }
+    else if (info.menuItemId === "ShareX_Upload_Text") {
+        chrome.runtime.sendNativeMessage(application, {
             Text: info.selectionText
         });
     }
