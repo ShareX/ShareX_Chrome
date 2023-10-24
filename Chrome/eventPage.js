9d2b7310a95c7506a7434a06180a9178f1e65432
Jaex <Jaex@users.noreply.github.com>
2023-10-24 08:13:07 +0300

    Added "Shorten URL with ShareX" button

--- Chrome/eventPage.js
+++ Chrome/eventPage.js
@@ -22,22 +22,47 @@ chrome.runtime.onInstalled.addListener((details) => {
         "title": "Upload text with ShareX",
         "contexts": ["selection"]
     });
+
+    chrome.contextMenus.create({
+        "id": "ShareX_Shorten_URL",
+        "title": "Shorten URL with ShareX",
+        "contexts": ["link"]
+    });
 });
 
 chrome.contextMenus.onClicked.addListener((info, tab) => {
     let application = "com.getsharex.sharex";
 
-    if (info.menuItemId === "ShareX_Upload_Image" ||
-        info.menuItemId === "ShareX_Upload_Video" ||
-        info.menuItemId === "ShareX_Upload_Video") {
-        chrome.runtime.sendNativeMessage(application, {
-            MediaType: info.mediaType,
-            URL: info.srcUrl
-        });
-    }
-    else if (info.menuItemId === "ShareX_Upload_Text") {
-        chrome.runtime.sendNativeMessage(application, {
-            Text: info.selectionText
-        });
+    switch (info.menuItemId) {
+        case "ShareX_Upload_Image":
+            chrome.runtime.sendNativeMessage(application, {
+                InputType: "image",
+                URL: info.srcUrl
+            });
+            break;
+        case "ShareX_Upload_Video":
+            chrome.runtime.sendNativeMessage(application, {
+                InputType: "video",
+                URL: info.srcUrl
+            });
+            break;
+        case "ShareX_Upload_Audio":
+            chrome.runtime.sendNativeMessage(application, {
+                InputType: "audio",
+                URL: info.srcUrl
+            });
+            break;
+        case "ShareX_Upload_Text":
+            chrome.runtime.sendNativeMessage(application, {
+                InputType: "text",
+                Text: info.selectionText
+            });
+            break;
+        case "ShareX_Shorten_URL":
+            chrome.runtime.sendNativeMessage(application, {
+                InputType: "link",
+                Link: info.linkUrl
+            });
+            break;
     }
 });
